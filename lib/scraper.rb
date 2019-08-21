ddcba16ca3b9e3a96c4e30a84f7312ba9919dddf
Annie Truex <artruex@gmail.com>
2019-08-21 22:45:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,6 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
-doc = Nokigiri::HTML(html)
+doc = Nokigiri::HTML(open("http://flatironschool.com/"))
+doc.css(".site-header__hero__headline")
 
